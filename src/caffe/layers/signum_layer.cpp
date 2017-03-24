b6ad7ccda293af34640890ea3ee8e251470af743
Jan Kohut <kohut.honza@seznam.cz>
2017-03-24 13:26:33 +0100

    CPU implementation of signum layer

--- src/caffe/layers/signum_layer.cpp
+++ src/caffe/layers/signum_layer.cpp
@@ -1,14 +1,9 @@
-#include <cmath>
 #include <vector>
 
+#include "caffe/util/math_functions.hpp"
 #include "caffe/layers/signum_layer.hpp"
 
 namespace caffe {
-
-template <typename Dtype>
-inline Dtype sigmoid(Dtype x) {
-  return 1. / (1. + exp(-x));
-}
 
 template <typename Dtype>
 void SignumLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
@@ -17,7 +12,7 @@ void SignumLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   Dtype* top_data = top[0]->mutable_cpu_data();
   const int count = bottom[0]->count();
   for (int i = 0; i < count; ++i) {
-    top_data[i] = sigmoid(bottom_data[i]);
+    top_data[i] = caffe_sign(bottom_data[i]);
   }
 }
 
@@ -26,13 +21,12 @@ void SignumLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
   if (propagate_down[0]) {
-    const Dtype* top_data = top[0]->cpu_data();
+    const Dtype* bottom_data = bottom[0]->cpu_data();
     const Dtype* top_diff = top[0]->cpu_diff();
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     const int count = bottom[0]->count();
     for (int i = 0; i < count; ++i) {
-      const Dtype sigmoid_x = top_data[i];
-      bottom_diff[i] = top_diff[i] * sigmoid_x * (1. - sigmoid_x);
+      bottom_diff[i] = top_diff[i] * (bottom_data[i] <= 1. && bottom_data[i] >= -1.);
     }
   }
 }

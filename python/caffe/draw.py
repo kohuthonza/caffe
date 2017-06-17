22a370f4343baaa23ebb95e8d1298ac87c9788f9
Jan Kohut <kohut.honza@seznam.cz>
2017-06-17 11:57:15 +0200

    Fix bugs + draw BinaryConvolution.

--- python/caffe/draw.py
+++ python/caffe/draw.py
@@ -49,7 +49,7 @@ def get_edge_label(layer):
 
     if layer.type == 'Data':
         edge_label = 'Batch ' + str(layer.data_param.batch_size)
-    elif layer.type == 'Convolution' or layer.type == 'Deconvolution':
+    elif layer.type == 'Convolution' or layer.type == "BinaryConvolution" or layer.type == 'Deconvolution':
         edge_label = str(layer.convolution_param.num_output)
     elif layer.type == 'InnerProduct':
         edge_label = str(layer.inner_product_param.num_output)
@@ -83,7 +83,7 @@ def get_layer_label(layer, rankdir):
         # horizontal space is not; separate words with newlines
         separator = '\\n'
 
-    if layer.type == 'Convolution' or layer.type == 'Deconvolution':
+    if layer.type == 'Convolution' or layer.type == "BinaryConvolution" or layer.type == 'Deconvolution':
         # Outer double quotes needed or else colon characters don't parse
         # properly
         node_label = '"%s%s(%s)%skernel size: %d%sstride: %d%spad: %d"' %\
@@ -104,11 +104,11 @@ def get_layer_label(layer, rankdir):
                       pooling_types_dict[layer.pooling_param.pool],
                       layer.type,
                       separator,
-                      layer.pooling_param.kernel_size[0] if len(layer.pooling_param.kernel_size._values) else 1,
+                      layer.pooling_param.kernel_size,
                       separator,
-                      layer.pooling_param.stride[0] if len(layer.pooling_param.stride._values) else 1,
+                      layer.pooling_param.stride,
                       separator,
-                      layer.pooling_param.pad[0] if len(layer.pooling_param.pad._values) else 0)
+                      layer.pooling_param.pad)
     else:
         node_label = '"%s%s(%s)"' % (layer.name, separator, layer.type)
     return node_label
@@ -120,6 +120,8 @@ def choose_color_by_layertype(layertype):
     color = '#6495ED'  # Default
     if layertype == 'Convolution' or layertype == 'Deconvolution':
         color = '#FF5050'
+    elif layertype == "BinaryConvolution":
+        color = '#42F4B6'
     elif layertype == 'Pooling':
         color = '#FF9900'
     elif layertype == 'InnerProduct':
